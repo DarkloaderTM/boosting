500c4253c012bb5180bbd8aef62d3ffe13ed2051
DarkloaderTM <94926122+DarkloaderTM@users.noreply.github.com>
2021-12-16 15:30:35 +0000

    further compatibility for np-base

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,11 +4,13 @@ games { 'gta5' }
 client_scripts {
   'config.lua',
   'client/cl_*.lua',
+  --'@unwind-rpc/client/cl_main.lua', --uncomment this line if using np-base
 }
 
 server_scripts {
   'config.lua',
   'server/sv_*.lua',
+  --'@unwind-rpc/server/sv_main.lua', --uncomment this line if using np-base
 }
 
 ui_page 'ui/index.html'
