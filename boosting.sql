6080873521f55c3e81605f2f7669297911af0341
Casper <71441748+JajarGaming@users.noreply.github.com>
2021-12-19 20:05:24 +0200

    Update boosting.sql

--- boosting.sql
+++ boosting.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `bropixel_boosting` (
+CREATE TABLE IF NOT EXISTS `boost_boosts` (
   `#` int(11) NOT NULL AUTO_INCREMENT,
   `citizenid` varchar(255) NOT NULL,
   `BNE` text NOT NULL DEFAULT '0',
